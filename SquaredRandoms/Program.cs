375a91cafe4680d1edef9ec91b26b2b28ada2f68
Michelle Beshears <meeshysu@gmail.com>
2019-03-11 19:53:34 -0500

    Add project files.

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
