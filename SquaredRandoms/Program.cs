0082fb360e30d5979525085042fc8a76caac66fd
Michelle Beshears <meeshysu@gmail.com>
2019-03-12 18:04:56 -0500

    it squares now but not on the separate line

--- SquaredRandoms/Program.cs
+++ SquaredRandoms/Program.cs
@@ -7,13 +7,8 @@ namespace SquaredRandoms
     {
         static void Main(string[] args)
         {
-            var randomNumberList = new List<int>();
-            Random random = new Random();
-
-            for (var i = 0; i < 20; i++)
-            {
-                Console.WriteLine(random.Next(1, 50));
-            }
+            var numberOfRandomness = new RandomGenerator();
+            numberOfRandomness.randomNumbers();
 
             Console.ReadLine();
         }
---
+++ SquaredRandoms/RandomGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredRandoms
+{
+    class RandomGenerator
+    {
+        Random random = new Random();
+        public List<int> randomNumberList = new List<int>();
+
+
+        public void randomNumbers()
+        {
+            var emptyString = "";
+            for (var i = 0; i < 20; i++)
+            {
+                var randomVariable = (random.Next(1, 50));
+                randomNumberList.Add(randomVariable);
+            }
+            foreach (var number in randomNumberList)
+            {
+                Console.Write($"{number},");
+            }
+            foreach (var number in randomNumberList)
+            {
+                var squaredNumber = number * number;
+                emptyString += squaredNumber.ToString();
+
+                Console.Write($"{squaredNumber}, ");
+            }
+        }
+    }
+}
